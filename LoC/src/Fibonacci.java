812dc8e547dcc0d4b7a166f479c41bab4f6e8eb0
indona <indona.vinita@gmail.com>
2016-05-22 15:17:22 -0700

    New milestone: 324B

--- LoC/src/Fibonacci.java
+++ LoC/src/Fibonacci.java
@@ -1,10 +1,1 @@
-import java.util.Scanner;
-public class Fibonacci{
-public static int fibonacci(int n){
-if(n==0|n==1)return 1;
-return fibonacci(n-1)+fibonacci(n-2);}
-
-public static void main(String args[]){
-int start=new Scanner(System.in).nextInt(),end=new Scanner(System.in).nextInt();
-for(int i=start;i<=end;i++)
-System.out.println(fibonacci(i));}}
+import java.util.Scanner;public class Fibonacci{public static int fibonacci(int n){if(n==0|n==1)return 1;return fibonacci(n-1)+fibonacci(n-2);}public static void main(String args[]){int start=new Scanner(System.in).nextInt(),end=new Scanner(System.in).nextInt();for(int i=start;i<=end;i++)System.out.println(fibonacci(i));}}
