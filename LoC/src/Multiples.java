8429339b6e58636a13f8ee3e8923660ce47562c8
indona <indona.vinita@gmail.com>
2016-05-21 18:30:43 -0700

    New milestone 122B

--- LoC/src/Multiples.java
+++ LoC/src/Multiples.java
@@ -1,7 +1,4 @@
-public class Multiples { 
-	public static void main (String args[]) {
-	    for (int i=3, j=0; j<100; i+=3, j++) {
-	        System.out.println(i);
-	    }
-	}
-}
+public class Multiples{
+public static void main (String args[]){
+for(int i=3,j=0;j<100;i+=3,j++)
+System.out.println(i);}}
