f5611e580010d77fe35bf802cceed44280531cae
indona <indona.vinita@gmail.com>
2016-05-22 15:13:11 -0700

    Improved: 334B

--- LoC/src/Fibonacci.java
+++ LoC/src/Fibonacci.java
@@ -1,21 +1,10 @@
 import java.util.Scanner;
+public class Fibonacci{
+public static int fibonacci(int n){
+if(n==0|n==1)return 1;
+return fibonacci(n-1)+fibonacci(n-2);}
 
-public class Fibonacci {
-	public static int fibonacci(int n) {
-		if (n==0)
-				return 0;
-		else if (n==1)
-			return 1;
-		else
-			return fibonacci(n-1)+fibonacci(n-2);
-	}
-	
-	public static void main (String args[]) {
-		int start = new Scanner(System.in).nextInt();
-		int end = new Scanner(System.in).nextInt();
-
-		for (int i=start; i<=end; i++) {
-			System.out.println(fibonacci(i));
-		}
-	}
-}
+public static void main(String args[]){
+int start=new Scanner(System.in).nextInt(),end=new Scanner(System.in).nextInt();
+for(int i=start;i<=end;i++)
+System.out.println(fibonacci(i));}}
