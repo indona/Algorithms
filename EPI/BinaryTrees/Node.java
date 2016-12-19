15928b55bef464b6244bd2b29f3dbff175a70957
indona <indona.vinita@gmail.com>
2016-12-19 00:38:26 -0800

    10.4 Compute the LCA when nodes have parent pointers

--- EPI/BinaryTrees/Node.java
+++ EPI/BinaryTrees/Node.java
@@ -6,11 +6,22 @@ public class Node
 
     //Added later
     int height;
+    Node parent;
 
+    //Original
     Node(int value)
     {
         this.value=value;
         left=null;
         right=null;
+    }
+
+    //Added Later
+    Node(int value, Node parent)
+    {
+        this.value=value;
+        left=null;
+        right=null;
+        this.parent=parent;
     }
 }
