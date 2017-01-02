fdbabaf5e2aa4beca700032f99d33a471821909b
indona <indona.vinita@gmail.com>
2017-01-01 20:54:02 -0800

    238. Product of Array Except Self - O(1) space

--- LeetCode/TheGrind/ProductExceptSelf.java
+++ LeetCode/TheGrind/ProductExceptSelf.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class ProductExceptSelf
 {
-    public static int[] productExceptSelf(int[] nums)
+/*    public static int[] productExceptSelf(int[] nums)
     {
         if(nums.length==0)
             return null;
@@ -17,21 +17,36 @@ public class ProductExceptSelf
         {
             t1[i]=t1[i-1]*nums[i-1];
         }
-        System.out.println("T1: "+Arrays.toString(t1));
 
         t2[nums.length-1]=1;
         for(int i=nums.length-2;i>=0;i--)
         {
             t2[i]=t2[i+1]*nums[i+1];
         }
-        System.out.println("T2: "+Arrays.toString(t2));
 
         for(int i=0;i<nums.length;i++)
         {
-            System.out.println("T1i: "+t1[i]+" T2i: "+t2[i]+"Result: "+result[i]);
             result[i]=t1[i]*t2[i];
         }
-        System.out.println("Result: "+Arrays.toString(result));
+        return result;
+    }
+*/
+    public static int[] productExceptSelf(int[] nums)
+    {
+        int[] result=new int[nums.length];
+        result[0]=1;
+
+        for(int i=1;i<nums.length;i++)
+        {
+            result[i]=result[i-1]*nums[i-1];
+        }
+
+        int temp=1;
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            result[i]=temp*result[i];
+            temp=temp*nums[i];
+        }
         return result;
     }
 
