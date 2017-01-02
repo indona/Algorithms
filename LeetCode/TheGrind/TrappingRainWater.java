2c5125e4a5a04fc5f78168dc603d681500159207
indona <indona.vinita@gmail.com>
2017-01-01 21:40:03 -0800

    42. Trapping Rain Water

---
+++ LeetCode/TheGrind/TrappingRainWater.java
@@ -0,0 +1,52 @@
+import java.lang.*;
+import java.util.*;
+
+class TrappingRainWater
+{
+    public static int trap(int[] height)
+    {
+        if (height.length==0 || height==null)
+            return 0;
+
+        //Idea is to scan from both ends and store the max height for that particular index from both ends. The minimum of the two tells you the volume for that cell
+        int[] left=new int[height.length];
+        int[] right=new int[height.length];
+
+        //Scan from left
+        int max=height[0];
+        for(int i=0;i<height.length;i++)
+        {
+            if(height[i]>max)
+                max=height[i];
+
+            left[i]=max;
+        }
+
+        //Scan from right
+        max=height[height.length-1];
+        for(int i=height.length-1;i>=0;i--)
+        {
+            System.out.println(i);
+            if(height[i]>max)
+                max=height[i];
+
+            right[i]=max;
+        }
+
+        //Calculate the volume (Minimum of left and right) and count the water that can be trapped i.e. Volume - height
+        int water=0, volume=0;
+        for(int i=0;i<height.length;i++)
+        {
+            volume=Math.min(left[i], right[i]);
+            water+=volume-height[i];
+        }
+
+        return water;
+    }
+
+    public static void main(String args[])
+    {
+        int[] height={0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println("Trapped water volume: "+trap(height));
+    }
+}
