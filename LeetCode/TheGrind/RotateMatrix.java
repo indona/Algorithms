e4124cdc1484bbbc0ff58cdabec0a7367cfb58d5
indona <indona.vinita@gmail.com>
2017-01-11 02:09:01 -0800

    Updating code for 48. Rotate matrix. Marking old file as obsolete.

--- LeetCode/TheGrind/RotateMatrix.java
+++ LeetCode/TheGrind/RotateMatrix.java
@@ -3,19 +3,59 @@ import java.lang.*;
 
 public class RotateMatrix
 {
-    public static void rotate(int[][] matrix)
+    //Rotating a matrix is equivalent to:
+    //1. Transpose
+    //2. Swapping of rows/ columns depending on the direction of rotation.
+    //For clockwise: Transpose + Column swap
+    //For Anti-clockwise: Transpose + Row swap
+
+    public static void rotateClockwise(int[][] matrix)
     {
-        //Basic formula: i,j <-- n-1-j, i
+        //Transpose
+        transpose(matrix);
+
+        //Swap columns (Left-Right swap)
         int n=matrix.length;
-        for(int i=0;i<n/2;i++)
+        for(int i=0;i<n;i++) //all rows
         {
-            for(int j=0;j<Math.ceil(((double)n)/2.);j++)
+            for(int j=0;j<n/2;j++) //half cols
             {
                 int temp=matrix[i][j];
-                matrix[i][j]=matrix[n-1-j][i];
-                matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
-                matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
-                matrix[j][n-1-i]=temp;
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+        }
+    }
+
+    public static void rotateAntiClockwise(int[][] matrix)
+    {
+        //Transpose
+        transpose(matrix);
+
+        //Swap rows (Top-Bottom swap)
+        int n=matrix.length;
+        for(int i=0;i<n/2;i++) //half rows
+        {
+            for(int j=0;j<n;j++) //all cols
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[n-i-1][j];
+                matrix[n-i-1][j]=temp;
+            }
+        }
+    }
+
+    public static void transpose(int[][] matrix)
+    {
+        //Only elements above the diagonal are swapped
+        int n=matrix.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
     }
@@ -23,8 +63,24 @@ public class RotateMatrix
     public static void main(String args[])
     {
         int[][] matrix={{1,2},{3,4}};
-        rotate(matrix);
+        System.out.println("Before");
         for(int[] row: matrix)
            System.out.println(Arrays.toString(row));
+
+        rotateClockwise(matrix);
+        System.out.println("After clockwise");
+        for(int[] row: matrix)
+           System.out.println(Arrays.toString(row));
+
+
+        int[][] matrix2={{1,2},{3,4}};
+        System.out.println("\nBefore");
+        for(int[] row: matrix2)
+            System.out.println(Arrays.toString(row));
+
+        System.out.println("After Anti-clockwise");
+        rotateAntiClockwise(matrix2);
+        for(int[] row: matrix2)
+            System.out.println(Arrays.toString(row));
     }
 }
