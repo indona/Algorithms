11ad45f8434eff6e67e6f0f1a513c687b1215396
indona <indona.vinita@gmail.com>
2017-01-23 01:15:00 -0800

    Using hashmap

--- LeetCode/TheGrind/LongestSubstring.java
+++ LeetCode/TheGrind/LongestSubstring.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class LongestSubstring
 {
-    public static int lengthOfLongestSubstring(String s)
+    public static int lengthOfLongestSubstring1(String s)
     {
         //The idea behind this problem is to scan the given string from left. We put the characters in a hashset. When we find a duplicate for the first time, keep track of the length of the substring and its end. Move the start by one and repeat the hashset addition till you find the next duplicate. Update the maxLength and end if you find a bigger substring.
         //A LinkedHashSet is used to maintain the order of insertion.
@@ -39,9 +39,32 @@ public class LongestSubstring
         return maxLength;
     }
 
+    public static int lengthOfLongestSubstring(String s)
+    {
+        //The idea behind this problem is to use a hasmap to keep track of the positions of each character in the string.
+        //In case of a duplicate, discard all values before the current entry of that character in the hashmap and reset the start to either the next element or the current start, which ever is bigger.
+        //Examples: "tmmzuxt" => for the 2nd 'm', start should be resetted to 2. Whereas for the second 't', start should stay at 2 as the index after the position of 1st t (i.e. 1) is smaller than the current start.
+
+        if(s==null || s.length()==0)
+            return 0;
+
+        HashMap<Character, Integer> map=new HashMap<Character, Integer>();
+        int start=0, maxLength=0, maxEnd=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(map.containsKey(s.charAt(i)))
+                start=Math.max(map.get(s.charAt(i))+1, start);
+
+            map.put(s.charAt(i), i);
+            maxLength=Math.max(maxLength, i-start+1);
+        }
+        System.out.println("Start: "+start+", End: "+(start+maxLength-1));
+        return maxLength;
+    }
+
     public static void main(String args[])
     {
-        String s="abcaxbcbdabcbb";
+        String s="tmmzuxt";
         System.out.println("Max length: "+lengthOfLongestSubstring(s));
     }
 }
