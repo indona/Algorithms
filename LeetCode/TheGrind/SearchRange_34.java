b11b8938f7e0c80389a12a6f2bccbe54029edf99
indona <indona.vinita@gmail.com>
2017-03-27 23:58:44 -0700

    34. Search for a Range

---
+++ LeetCode/TheGrind/SearchRange_34.java
@@ -0,0 +1,42 @@
+public class SearchRange_34 
+{
+    public int[] searchRange(int[] nums, int target)
+    {
+        int start=0;
+        int end=nums.length-1;
+
+        int[] range = {nums.length, -1};
+        _searchRange(nums, target, range, start, end);
+
+        if(range[0]>range[1])
+            return (new int[]{-1, -1});
+        else
+            return range;
+    }
+
+    public void _searchRange(int[] nums, int target, int[] range, int start, int end)
+    {
+        int mid=(start+end)/2;
+        if(start<=end)
+        {
+            if(nums[mid]>target)
+                _searchRange(nums, target, range, start, mid-1);
+            else if (nums[mid]<target)
+                _searchRange(nums, target, range, mid+1, end);
+            else //equal
+            {
+                if(mid<range[0])
+                {
+                    range[0]=mid;
+                    _searchRange(nums, target, range, start, mid-1);
+                }
+                if(mid>range[1])
+                {
+                    range[1]=mid;
+                    _searchRange(nums, target, range, mid+1, end);
+                }
+            }
+        }
+        else return;
+    }
+}
